ce355abb7e8cb7a185eab0b8f9372af3d1e68f55
Virginia Mesa Pérez <virmesper@alum.us.es>
2026-02-16 14:43:36 +0100

    Requisitos de información 1

---
+++ src/main/java/acme/entities/invention/InventionRepository.java
@@ -0,0 +1,15 @@
+
+package acme.entities.invention;
+
+import org.springframework.data.jpa.repository.Query;
+import org.springframework.stereotype.Repository;
+
+import acme.client.repositories.AbstractRepository;
+
+@Repository
+public interface InventionRepository extends AbstractRepository {
+
+	@Query("select i from Invention i where i.id = :id")
+	Invention findOneInventionById(int id);
+
+}
---
+++ src/main/java/acme/entities/part/Part.java
@@ -0,0 +1,51 @@
+
+package acme.entities.part;
+
+import javax.persistence.Column;
+import javax.persistence.Entity;
+import javax.persistence.ManyToOne;
+import javax.validation.Valid;
+
+import acme.client.components.basis.AbstractEntity;
+import acme.client.components.datatypes.Money;
+import acme.client.components.validation.Mandatory;
+import acme.client.components.validation.ValidMoney;
+import acme.constraints.ValidHeader;
+import acme.constraints.ValidText;
+import acme.entities.invention.Invention;
+import lombok.Getter;
+import lombok.Setter;
+
+@Entity
+@Getter
+@Setter
+public class Part extends AbstractEntity {
+
+	private static final long	serialVersionUID	= 1L;
+
+	@Mandatory
+	@ValidHeader
+	@Column(name = "name")
+	public String				name;
+
+	@Mandatory
+	@ValidText
+	@Column(name = "description")
+	public String				description;
+
+	@Mandatory
+	@ValidMoney(min = 0)
+	@Column(name = "cost")
+	public Money				cost;
+
+	@Mandatory
+	@Valid
+	@Column(name = "kind")
+	public PartKind				kind;
+
+	@Mandatory
+	@Valid
+	@ManyToOne
+	public Invention			invention;
+
+}
