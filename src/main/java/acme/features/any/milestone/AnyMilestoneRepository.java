621a481e1ea2a5210a119c71520d89919ad2a304
martadelacalle <martadelacalle2003@gmail.com>
2026-03-02 16:58:39 +0100

    feat: any milestone from campaign

---
+++ src/main/java/acme/features/any/milestone/AnyMilestoneRepository.java
@@ -0,0 +1,25 @@
+
+package acme.features.any.milestone;
+
+import java.util.Collection;
+
+import org.springframework.data.jpa.repository.Query;
+import org.springframework.stereotype.Repository;
+
+import acme.client.repositories.AbstractRepository;
+import acme.entities.campaign.Campaign;
+import acme.entities.milestone.Milestone;
+
+@Repository
+public interface AnyMilestoneRepository extends AbstractRepository {
+
+	@Query("select m from Milestone m where m.id = :id")
+	Milestone findMilestoneById(int id);
+
+	@Query("select c from Campaign c where c.id = :campaignId")
+	Campaign findCampaignById(int campaignId);
+
+	@Query("select m from Milestone m where m.campaign.id = :campaignId")
+	Collection<Milestone> findAllMilestonesByCampaignId(int campaignId);
+
+}
