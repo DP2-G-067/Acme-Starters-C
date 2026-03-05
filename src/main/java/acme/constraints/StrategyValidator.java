a37b50b19ef041cdccb09a1a5592fd4930a7050f
JuanAntonioMorenoMoguel <juamormog@gmail.com>
2026-03-05 18:53:23 +0200

    26.3.0

--- src/main/java/acme/constraints/StrategyValidator.java
+++ src/main/java/acme/constraints/StrategyValidator.java
@@ -29,63 +29,55 @@ import acme.entities.tactic.Tactic;
 @Validator
 public class StrategyValidator extends AbstractValidator<ValidStrategy, Strategy> {
 
-	// Internal state ---------------------------------------------------------
-
 	@Autowired
 	private StrategyRepository repository;
 
-	// ConstraintValidator interface ------------------------------------------
-
 
 	@Override
-	protected void initialise(final ValidStrategy annotation) {
-		assert annotation != null;
-	}
-
-	@Override
-	public boolean isValid(final Strategy strategy, final ConstraintValidatorContext context) {
-		// HINT: job can be null
+	public boolean isValid(final Strategy value, final ConstraintValidatorContext context) {
 		assert context != null;
-
 		boolean result;
-
-		if (strategy == null)
-			result = true;
-		else {
-			{
-				boolean uniqueStrategy;
-				Strategy existingStrategy;
-
-				existingStrategy = this.repository.findStrategyByTicker(strategy.getTicker());
-				uniqueStrategy = existingStrategy == null || existingStrategy.equals(strategy);
-
-				super.state(context, uniqueStrategy, "ticker", "acme.validation.strategy.duplicated-ticker.message");
-			}
-			{
-				boolean isFutureStart = true;
-				boolean isFutureEnd = true;
-				Date startDate = strategy.getStartMoment();
-				Date endDate = strategy.getEndMoment();
-
-				if (!strategy.getDraftMode()) {
-					isFutureStart = MomentHelper.isAfter(startDate, MomentHelper.getCurrentMoment());
-					isFutureEnd = MomentHelper.isAfter(endDate, startDate);
-				}
-				super.state(context, isFutureStart, "startMoment", "acme.validation.strategy.startMoment.message");
-				super.state(context, isFutureEnd, "endMoment", "acme.validation.strategy.endMoment.message");
-			}
-			{
-				boolean isPublishedWithNoTactics = false;
-				Collection<Tactic> tactics = this.repository.findTacticsByStrategyId(strategy.getId());
-
-				if (tactics.isEmpty() && !strategy.getDraftMode())
-					isPublishedWithNoTactics = true;
-				super.state(context, isPublishedWithNoTactics, "*", "acme.validation.strategy.published-with-no-tactics");
-			}
-			result = !super.hasErrors(context);
-		}
-
+		if (value == null)
+			return true;
+		else
+			result = this.isUnique(value, context) && this.correctStatus(value, context) && this.isFutureDate(value, context);
 		return result;
 	}
 
+	private boolean isUnique(final Strategy strategy, final ConstraintValidatorContext context) {
+		boolean uniqueStrategy;
+		Strategy existingStrategy;
+
+		existingStrategy = this.repository.findStrategyByTicker(strategy.getTicker());
+		uniqueStrategy = existingStrategy == null || existingStrategy.equals(strategy);
+
+		super.state(context, uniqueStrategy, "ticker", "acme.validation.strategy.duplicated-ticker.message");
+		return !super.hasErrors(context);
+	}
+
+	// Can not be published unless they have one milestone. 
+	private boolean correctStatus(final Strategy strategy, final ConstraintValidatorContext context) {
+		boolean correctStatus = true;
+		Collection<Tactic> tacticByStrategy = this.repository.findTacticsByStrategyId(strategy.getId());
+		if (!strategy.getDraftMode())
+			correctStatus = tacticByStrategy.size() > 0;
+
+		super.state(context, correctStatus, "draftMode", "acme.validation.strategy.draftMode.message");
+		return !super.hasErrors(context);
+	}
+
+	private boolean isFutureDate(final Strategy strategy, final ConstraintValidatorContext context) {
+		boolean isFutureStart = true;
+		boolean isFutureEnd = true;
+		Date startDate = strategy.getStartMoment();
+		Date endDate = strategy.getEndMoment();
+
+		if (!strategy.getDraftMode()) {
+			isFutureStart = MomentHelper.isAfter(startDate, MomentHelper.getCurrentMoment());
+			isFutureEnd = MomentHelper.isAfter(endDate, startDate);
+		}
+		super.state(context, isFutureStart, "startMoment", "acme.validation.strategy.startMoment.message");
+		super.state(context, isFutureEnd, "endMoment", "acme.validation.strategy.endMoment.message");
+		return !super.hasErrors(context);
+	}
 }
