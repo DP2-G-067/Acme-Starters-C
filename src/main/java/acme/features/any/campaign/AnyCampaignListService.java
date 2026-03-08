afaada2b5bed9c61cdb279640e877fa188e75258
martadelacalle <martadelacalle2003@gmail.com>
2026-03-08 22:39:32 +0100

    fix: object creations

--- src/main/java/acme/features/any/campaign/AnyCampaignListService.java
+++ src/main/java/acme/features/any/campaign/AnyCampaignListService.java
@@ -35,6 +35,6 @@ public class AnyCampaignListService extends AbstractService<Any, Campaign> {
 
 	@Override
 	public void unbind() {
-		super.unbindObjects(this.campaigns, "name", "description", "startMoment", "endMoment", "moreInfo");
+		super.unbindObjects(this.campaigns, "name", "description", "startMoment", "endMoment", "moreInfo", "spokesperson.name");
 	}
 }
